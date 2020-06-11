dc303dab29f2d5a8b6c1d0ca9017914d2beb63fd
D.N.Carter <duncancarter93@gmail.com>
2020-06-11 18:03:01 +0100

    adding CSS file for Hello component

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import './Hello.css';
 
-class Hello extends Component {
+class Hello extends React.Component {
 	render() {
-		return <h1>Hello World</h1>
+		return (
+			<div className='f1 tc'>
+				<h1>Hello World</h1>
+				<p>This is my first React app</p>
+			</div>
+			)
 	}
 }
+
+export default Hello;
