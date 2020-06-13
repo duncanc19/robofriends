006200326a4a9687238ad3f2b92c78d1dbf4c4c9
D.N.Carter <duncancarter93@gmail.com>
2020-06-13 11:10:37 +0100

    Added SearchBox which filters cards

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,6 @@ const Card = ({ name, email, id }) => {
 	const { name, email, id } = props; */}
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-		  <h1>RoboFriends</h1>
 		  <img src= {`https://robohash.org/${id}?size=200x200`} alt='robots' />
 		  <div>
 		  	<h2>{name}</h2>
