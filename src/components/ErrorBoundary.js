6d40f149f1a7c23b8c432562d66495bdb18d8015
D.N.Carter <duncancarter93@gmail.com>
2020-06-15 15:29:41 +0100

    Added Github pages

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -14,7 +14,7 @@ class ErrorBoundary extends Component {
 
 	render() {
 		if (this.state.hasError) {
-			return <h1>Sorry, there's an error :(</h1>;
+			return <h1>Sorry, there's an error :(</h1>
 		}
 		return this.props.children;
 	}
