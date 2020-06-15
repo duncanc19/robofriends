0325ad49882d629f79f2e4101b5678a57553b201
D.N.Carter <duncancarter93@gmail.com>
2020-06-15 09:51:34 +0100

    Some refactoring and run build

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Card = ({ name, email, id }) => {
-	{/* can do destructuring in the function parameters instead of after like this:
-	const { name, email, id } = props; */}
+{/* can do destructuring in the function parameters instead of after like this:
+const { name, email, id } = props; */}
 	return (
 		<div className='tc bg-light-blue dib br3 pa2 ma2 grow bw2 shadow-5'>
 		  <img src= {`https://robohash.org/${id}?size=200x200`} alt='robots' />
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import { robots } from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 
@@ -25,17 +25,24 @@ class App extends Component {
 	}
 
 	render() {
-	  const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+	  const { robots, searchField } = this.state;
+	  const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 
-	  return (
+	  if (!robots.length) {
+	  	return <h1>Loading</h1>
+	  } else {
+	  	return (
 		  <div className='tc'>
 		    <h1 className='f1'>RoboFriends</h1>
 		    <SearchBox searchChange={this.onSearchChange} />
-		    <CardList robots={filteredRobots} />
+		    <Scroll>
+		      <CardList robots={filteredRobots} />
+		    </Scroll>
 		  </div>
-	  )	
+	    )	
+	  }  
 	}
 }
 
